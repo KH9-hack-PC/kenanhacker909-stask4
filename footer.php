178ad22bc371f4362f28e2c075e28b72bb199ed0
KH9-hack-PC <909Passhack@gmail.com>
2021-12-28 20:17:53 +0400

    kenan

---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php?>
+<div class="footer">
+    <div class="row">
+        <div class="col-12 col-lg-6 col-xl-6">
+            <img width="400px" height="400px" src="images/iphone4.png" alt="This Is An iPhone 4">
+        </div>
+        <div class="col-12 col-lg-6 col-xl-6">
+            <p class="p1">Professionals          Responsive</p>
+            <p class="p1">Lorem, ipsum dolor sit amet consectetur adipisicing elit.          Eaque sint totam maxime hic blanditiis esse,
+                nihil similique quisquam, at quae, eum nostrum pariatur          repudiandae numquam! Illo commodi eligendi</p>
+        </div>
+    </div>
+</div>
+</div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+    integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+</script>
+
+</html>
+<?php?>
